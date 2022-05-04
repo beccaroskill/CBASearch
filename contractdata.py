564ad284f6d22fc4608ab430c4e49d6b497e4368
beccaroskill <beccaroskill@icloud.com>
2022-05-04 17:51:04 -0400

    Adding NCAIS parsing for industry filtering

--- contractdata.py
+++ contractdata.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 import pandas as pd
+import json
 
 class ContractLine:
     
@@ -13,16 +14,18 @@ class ContractLine:
         
 class ContractSearchResult:
     
-    def __init__(self, contract_name, lines, result_line_index):
+    def __init__(self, contract_name, contract_industry, lines, result_line_index):
         self.contract_name = contract_name
+        self.contract_industry = contract_industry
         self.lines = lines
         self.result_line_index = result_line_index
 
 class ContractDatabase:
     
-    def __init__(self, contract_text_folder, contract_metadata_file):
+    def __init__(self, contract_text_folder, contract_metadata_file, naics_structure_file):
         self.contract_text_db = self.load_contract_text(contract_text_folder)
         self.contract_metadata_db = pd.read_csv(contract_metadata_file)
+        self.naics_structure_db = pd.read_csv(naics_structure_file)
         
     def load_contract_text(self, contract_text_folder):
         lines_all = []
@@ -36,20 +39,77 @@ class ContractDatabase:
         df = pd.DataFrame(lines_all, columns=['text', 'is_header', 'contract_id', 'line_index'])
         return df
     
+    def get_industries(self):
+        codes = self.naics_structure_db['2022 NAICS Code']
+        titles = self.naics_structure_db['2022 NAICS Title']
+        industries = [{"name": "All industries", "sub_industries":[{"name": "All industries",
+                                                                    "codes": "[]"}]}]
+        industry=None
+        for code, title in zip(codes,titles):
+            code = str(code)
+            if title==title:
+                title = str(title)
+                trim_index = title.find('T', len(title)-3, len(title))
+                title_trimmed = title[:trim_index] if trim_index>0 else title
+                codes = code.split('-')
+                if len(codes[0])==2:
+                    if industry:
+                        industries.append(industry)
+                    industry = {"name": title_trimmed, 
+                                "sub_industries":[{"name":"All " + title_trimmed,
+                                                   "codes": [int(c) for c in codes]}]}
+                elif len(codes[0])==3:
+                    industry["sub_industries"].append({"name": title_trimmed, 
+                                                       "codes": [int(c) for c in codes]})
+        return industries
+    
     def get_contract_name(self, contract_id):
         id_matches = self.contract_metadata_db[self.contract_metadata_db['CBA File']==int(contract_id)]['Union']
-        if len(id_matches):
-            return id_matches.tolist()[0]
-        else:
-            return "???"
+        assert len(id_matches) > 0, \
+            "Invalid value for contract_id, no contracts match in database."
+        return id_matches.tolist()[0]
     
-    def get_search_results(self, search_term):
+    def get_contract_link(self, contract_id):
+        id_matches = self.contract_metadata_db[self.contract_metadata_db['CBA File']==int(contract_id)]
+        assert len(id_matches) > 0, \
+            "Invalid value for contract_id, no contracts match in database."
+        return f"https://olmsapps.dol.gov/olpdr/GetAttachmentServlet?docId={contract_id}"
+        
+    def get_contract_sector(self, contract_id):
+        id_matches = self.contract_metadata_db[self.contract_metadata_db['CBA File']==int(contract_id)]['Type']
+        assert len(id_matches) > 0, \
+            "Invalid value for contract_id, no contracts match in database."
+        return id_matches.tolist()[0]
+            
+    def get_contract_industry(self, contract_id):
+        id_matches = self.contract_metadata_db[self.contract_metadata_db['CBA File']==int(contract_id)]['NAICS*']
+        assert len(id_matches) > 0, \
+            "Invalid value for contract_id, no contracts match in database."
+        return id_matches.tolist()[0]
+    
+    def filter_search_results(search_results, filters):
+        filter_industry_codes = json.loads(filters['industry_codes'])
+        filtered_results = []
+        for result in search_results:
+            if len(filter_industry_codes):
+                for code in filter_industry_codes:
+                    code = str(code)
+                    if result.contract_industry == result.contract_industry:
+                        result_industry = str(int(result.contract_industry))
+                        if result_industry[:len(code)]==code:
+                            filtered_results.append(result)
+            else:
+                filtered_results.append(result)
+        return filtered_results
+    
+    def get_search_results(self, search_term, filters):
         matches = self.contract_text_db[self.contract_text_db['text'].str.contains(search_term, case=False)]
         search_results = []
         for _, match in matches.iterrows():
             line_index = match['line_index']
             contract_id = match['contract_id']
             contract_name = self.get_contract_name(contract_id)
+            contract_industry = self.get_contract_industry(contract_id)
             contract_lines = self.contract_text_db[self.contract_text_db['contract_id']==contract_id]
             line_range_min = max(0, line_index-3)
             line_range_max = min(line_index+3, len(contract_lines))
@@ -60,9 +120,10 @@ class ContractDatabase:
                 line = ContractLine(match_adj['text'], match_adj['is_header'], match_adj['contract_id'], 
                                     match_adj['line_index'], match_adj['line_index']==line_index)
                 lines.append(line)
-            search_result = ContractSearchResult(contract_name, lines, line_index)
+            search_result = ContractSearchResult(contract_name, contract_industry, lines, line_index)
             search_results.append(search_result)
-        return search_results
+        filtered_results = ContractDatabase.filter_search_results(search_results, filters)
+        return filtered_results
         
     def lines_to_response(lines, contract_id):
         response = []
--- main.py
+++ main.py
@@ -10,8 +10,23 @@ from contractdata import ContractDatabase
 
 app = FastAPI(title="CBA Search")
 app.mount("/static", StaticFiles(directory="static"), name="static")
-contract_db = ContractDatabase("data/DOL_Scrape/ContractText", "data/CBAList.csv")
+contract_db = ContractDatabase("data/DOL_Scrape/ContractText", 
+                               "data/CBAList.csv",
+                               "data/2022_NAICS_Structure.csv")
 templates = Jinja2Templates(directory="templates/")
+
+def get_context(request, search_results):
+    return {"request": request, 
+            "industries": contract_db.get_industries(),
+            "topics": ['All topics',
+                       'Unit definition',
+                       'Compensation',
+                       'Healthcare',
+                       'Leaves & Absences'],
+            "sectors": ['All sectors (Public/Private)',
+                        'Private',
+                        'Public'],
+            "results": search_results}
 
 @app.get("/", response_class=HTMLResponse)
 async def read_items():
@@ -30,12 +45,13 @@ async def read_items():
 def form_post(request: Request):
     search_results = []
     return templates.TemplateResponse(
-        "search.html", context={"request": request, "results": search_results}
+        "search.html", context=get_context(request, search_results)
     )
 
 @app.post("/search")
-def form_post(request: Request, contract_id: str = Form(...)):
-    search_results = contract_db.get_search_results(contract_id)
+def form_post(request: Request, search_term: str = Form(...), industry_codes: str = Form(...)):
+    search_filters = {"industry_codes": industry_codes}
+    search_results = contract_db.get_search_results(search_term, search_filters)
     return templates.TemplateResponse(
-        "search.html", context={"request": request, "results": search_results}
+        "search.html", context=get_context(request, search_results)
     )
