dbf18ea0be0d5574034188e955476ec469259f1d
beccaroskill <beccaroskill@icloud.com>
2022-05-07 20:29:50 -0400

    Adding multiprocessing to read script

--- cba_search/data/readcontracts.py
+++ cba_search/data/readcontracts.py
@@ -1,11 +1,18 @@
-from pdf2image import convert_from_path
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  6 15:23:33 2022
+
+@author: beccaroskill
+"""
 import pytesseract
+from pdf2image import convert_from_path, pdfinfo_from_path
 import cv2
 import xml.etree.ElementTree as ET
-import numpy as np
-from tqdm import tqdm
 import os
 from pathlib import Path
+from multiprocessing import Pool
+
 
 def get_bbox(hocr_title):
     bbox_strs = hocr_title.split(' ')[1:5]
@@ -20,22 +27,30 @@ def get_wconf(hocr_title):
     return 0
 
 def pdf_to_imgs(pdf_path, out_path, out_name):
-    pages = convert_from_path(pdf_path, dpi=500)
+    info = pdfinfo_from_path(pdf_path)
+    pages = info['Pages']
+    page_block_size = 100
+    n_full_groups = pages//page_block_size
+    page_ranges = [(i*page_block_size, (i+1)*page_block_size-1) for i in range(n_full_groups)] + \
+                  [(page_block_size*n_full_groups, pages)]
     img_paths = []
-    out_path_full = os.path.join(out_path, out_name)
-    for i, page in enumerate(pages):
-        img_path = '{}_{}.jpg'.format(out_path_full, i)
-        page.save(img_path, 'JPEG')
-        img_paths += [img_path]
+    for j, (first_page, last_page) in enumerate(page_ranges):
+      pages = convert_from_path(pdf_path, dpi=500, first_page=first_page, last_page=last_page)
+      out_path_full = os.path.join(out_path, out_name)
+      for i, page in enumerate(pages):
+          img_path = '{}_{}.jpg'.format(out_path_full, i+page_block_size*j)
+          page.save(img_path, 'JPEG')
+          img_paths += [img_path]
     return img_paths
 
 def imgs_to_md(img_paths):
     doc_lines = []
-    for img_path in tqdm(img_paths):
+    for img_path in img_paths:
         img = cv2.imread(img_path)    
         # Get HOCR output
         hocr = pytesseract.image_to_pdf_or_hocr(img, extension='hocr')
         root = ET.fromstring(hocr)
+        prev_line_article_header = False
         for line in root.iter():
             if 'id' in line.attrib and 'line' in line.attrib['id']:
                 word_texts = []
@@ -45,38 +60,55 @@ def imgs_to_md(img_paths):
                         if word_conf > 10:
                             word_texts.append(word.text)
                 line_text = ' '.join(word_texts)
-                if is_header(line_text):
+                if is_header(line_text, prev_line_article_header):
                     doc_lines.append('### ' + line_text)
                 else:
                     doc_lines.append(line_text)
+                prev_line_article_header = is_article_header(line_text)
     doc_text = '  \n'.join(doc_lines)
     return doc_text
-                    
-def is_header(text):
-    begins_upper = get_count_upper(text[:15]) > 0.3 * 15
+
+def is_header(text, prev_line_article_header=False):
+    is_upper = get_count_upper(text) > 0.5 * len(text)
     words = [c for c in text.split()]
-    is_title = sum([1 for c in words if c[0].isupper()]) > 0.5 * len(words)
-    return begins_upper or is_title
+    is_article_title = prev_line_article_header and sum([1 for c in words if c[0].isupper()]) > 0.5 * len(words)
+    return is_upper or is_article_title
+
+def is_article_header(text):
+    words = [c for c in text.split()]
+    words_lower = [c.lower() for c in words]
+    is_article_header = ('article' in words_lower)
+    return is_article_header
 
 def get_count_upper(text):
     count_upper = sum([1 for c in text if c.isupper()])
     return count_upper
 
-contracts_folder = 'Contracts'
-imgs_folder = 'ContractImages'
-txt_folder = 'ContractText'
+contracts_folder = 'DOL_Scrape/Contracts'
+imgs_folder = 'DOL_Scrape/ContractImages'
+txt_folder = 'DOL_Scrape/ContractText_LayoutParser'
 
-for file_path in os.listdir(contracts_folder): 
-    img_paths = []
+def read_contract_from_path(file_path):
     file_ext = Path(file_path).suffix
     if file_ext == '.pdf':
-        pdf_path = os.path.join(contracts_folder, file_path)
-        contract_id = file_path[:file_path.index(file_ext)]
-        if not '{}.txt'.format(contract_id) in os.listdir(txt_folder):
-            print('Reading', file_path)
-            img_paths += [pdf_to_imgs(pdf_path, imgs_folder, contract_id)]
-            for contract_img_paths in img_paths:
+        try:
+            pdf_path = os.path.join(contracts_folder, file_path)
+            contract_id = file_path[:file_path.index(file_ext)]
+            if not '{}.txt'.format(contract_id) in os.listdir(txt_folder):
+                print('Reading', file_path)
+                img_paths = pdf_to_imgs(pdf_path, imgs_folder, contract_id)
                 text_file_path = '{}/{}.txt'.format(txt_folder, contract_id)
-                doc_text = imgs_to_md(contract_img_paths)
+                doc_text = imgs_to_md(img_paths)
                 with open(text_file_path, "w") as text_file:
-                    n = text_file.write(doc_text)
+                    text_file.write(doc_text)
+        except Exception as e:
+            print(e)
+
+def main():
+    with Pool(5) as p:
+        p.map(read_contract_from_path, list(os.listdir(contracts_folder)))
+
+if __name__ == "__main__":
+    main() 
+
+    
